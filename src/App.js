7c607a1901c09cdec3aba718bc149e33091d9545
nawazzz <65383832+nawazzz@users.noreply.github.com>
2021-03-09 12:37:52 +0530

    add functionality to react counter app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import "./App.scss";
 import React from 'react';
+import "./App.scss";
+import List from "./List";
+import icon from "./icon.jpg"
 
-function App() {
-  return (
-    <React.Fragment>
-      test
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+  increment = (e) => {
+    if (e.type === "click") {
+      this.setState({
+        count: this.state.count + 1
+      })    
+    }
+  }
+  decrement = (e) => {
+    if (e.type === "click") {
+      this.setState({
+        count: this.state.count - 1
+      })    
+    }
+  }
+
+  render() {
+      return(
+        <React.Fragment>
+          <div>
+            <div>
+              <div>
+                {this.state.count}
+              </div>
+              <div onClick={this.increment} style={{width: "100px"}}>
+                +
+              </div>
+              <div onClick={this.decrement}>
+                -
+              </div>
+              <div>
+                <img src = {icon}/>
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      )
+    }
 }
-
 export default App;
